f94640a23a1cd58b12b4d9856bf60aeceee6c839
SashaKuh <71312391+SashaKuh@users.noreply.github.com>
2023-04-26 16:51:39 +0300

    fix

--- src/js/to-top.js
+++ src/js/to-top.js
@@ -15,7 +15,7 @@ let calcScrollValue = () => {
     scrollProgress.addEventListener("click", () => {
       document.documentElement.scrollTop = 0;
     });
-    scrollProgress.style.background = `conic-gradient(#03cc65 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+    scrollProgress.style.background = `conic-gradient(#004D33 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
   };
   
   window.onscroll = calcScrollValue;
